b47192ad87534b5807e10a631c339438bfe8a84e
lukban90 <lukban4c@gmail.com>
2019-08-04 21:15:07 -0700

    refactored code | added spinner gif/Users component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
 import "./App.css";
 import NavBar from "./components/layout/NavBar";
-import UserItem from "./components/users/UserItem";
+import Users from "./components/users/Users";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const data = await fetch("https://api.github.com/users").then(response =>
+      response.json()
+    );
+    console.log(data);
+    this.setState({ users: data, loading: false });
+  }
+
   render() {
+    const { loading, users } = this.state;
     return (
-      <div className='App'>
+      <div className="App">
         <NavBar />
-        <UserItem />
+        <div className="container">
+          <Users loading={loading} users={users} />
+        </div>
       </div>
     );
   }
